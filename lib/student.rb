a78d93b57b5814b7681a8c370d439c7b122d0b91
James Pier <jpier2012@gmail.com>
2019-05-09 12:56:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,11 +28,5 @@ end
 stud_hash = {:name => "Jimmers", :location => "Round Lake", :profile_quote => "'Pikachu' - Pikachu"}
 
 stud = Student.new(stud_hash)
-puts "------------------"
-puts "methods"
-puts "------------------"
-puts stud.methods
-puts "------------------"
-puts "attributes"
-puts "------------------"
-puts stud.class.attributes
+
+puts stud.class.methods
