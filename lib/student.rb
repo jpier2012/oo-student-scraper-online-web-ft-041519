09fb8143cd0ad32609f6d0ce5877b7aaa6a8748b
James Pier <jpier2012@gmail.com>
2019-05-09 13:15:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,13 +20,8 @@ class Student
   def self.create_from_collection(students_array)
     # iterates through each element of the array
     students_array.each do |student|
-      # iterates through hash key/value pairs
-      student.each do |key, value|
-        # creates a new student, then runs the code block to mass assign attributes
-        self.new.tap do |s|
-          s.send("#{key}=", value)
-        end
-      end
+      # creates a new student from the passed hash
+      self.new(student)
     end
   end
 
